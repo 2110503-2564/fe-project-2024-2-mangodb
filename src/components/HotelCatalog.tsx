ed653af9d954d46dc4123ace1de0b0ec290a73e6
Chandan Kanyarat <6733029621@student.chula.ac.th>
2025-03-21 11:29:31 +0700

    Modified HotelCard

--- src/components/HotelCatalog.tsx
+++ src/components/HotelCatalog.tsx
@@ -31,13 +31,11 @@ export default function HotelCatalog(){
               hid="12345" />
           </Link>
         ))} */}
-        <Link href={`/room/`} className="w-1/5">
-            <Card venueName="Bangkok Hotel"
-              imgSrc="your-image-url.jpg"
+            <Card hotelName="Bangkok Hotel"
+              imgSrc="/img/grandtable.jpg"
               location="Bangkok, Thailand"
               rating={5.0}
               hid="12345" />
-        </Link>
 
       </div>
     </>
--- src/components/InteractiveCard.tsx
+++ src/components/InteractiveCard.tsx
@@ -1,8 +1,16 @@
 "use client";
 
-export default function Card({ children }: { children: React.ReactNode }) {
+import React from "react";
+
+export default function InteractiveCard({ 
+  children, 
+  onClick 
+}: { 
+  children: React.ReactNode; 
+  onClick?: () => void;
+}) {
   function onCardMouseAction(event: React.SyntheticEvent) {
-    if (event.type == "mouseover") {
+    if (event.type === "mouseover") {
       event.currentTarget.classList.remove("shadow-lg", "bg-white");
       event.currentTarget.classList.add("shadow-2xl", "bg-neutral-200");
     } else {
@@ -10,11 +18,13 @@ export default function Card({ children }: { children: React.ReactNode }) {
       event.currentTarget.classList.add("shadow-lg", "bg-white");
     }
   }
+
   return (
     <div
-      className="h-[300px] rounded-lg shadow-lg bg-white"
+      className="rounded-lg shadow-lg bg-white cursor-pointer transition-all duration-300 overflow-hidden"
       onMouseOver={(e) => onCardMouseAction(e)}
       onMouseOut={(e) => onCardMouseAction(e)}
+      onClick={onClick}
     >
       {children}
     </div>
