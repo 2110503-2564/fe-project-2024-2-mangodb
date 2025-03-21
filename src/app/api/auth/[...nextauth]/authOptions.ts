fe7711537eb92152c3a4f84c60bc1f733aafec9c
Mantawit Ketphan <mantawit999@gmail.com>
2025-03-22 01:25:29 +0700

    Edit

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -8,8 +8,8 @@ export const authOptions: AuthOptions = {
     CredentialsProvider({
       // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // credentials is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the credentials object.
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
@@ -21,7 +21,7 @@ export const authOptions: AuthOptions = {
         const user = await userLogIn(credentials.email, credentials.password);
 
         if (user) {
-          // Any object returned will be saved in `user` property of the JWT
+          // Any object returned will be saved in user property of the JWT
           return user;
         } else {
           // If you return null then an error will be displayed advising the user to check their details.
@@ -32,6 +32,7 @@ export const authOptions: AuthOptions = {
   ],
   pages: {
     signIn: "/auth/signin",
+    signOut: "/auth/signout"
   },
   session: { strategy: "jwt" },
   callbacks: {
