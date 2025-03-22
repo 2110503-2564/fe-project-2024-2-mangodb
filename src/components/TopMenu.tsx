7c19b6a04d4475a73135d7a9885c4bb872562815
Baworntatt Taeprasert <baworntatt.t@gmail.com>
2025-03-22 23:46:22 +0700

    add z index to topmenu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -11,29 +11,55 @@ export default async function TopMenu() {
   const session = await getServerSession(authOptions);
 
   return (
-    <nav className={`flex items-center justify-between px-8 py-2 bg-white shadow-md fixed top-0 right-0 left-0`}>
+    <nav
+      className={`flex items-center justify-between px-8 py-2 bg-white shadow-md fixed top-0 right-0 left-0 z-40`}
+    >
       {/* Left Section: Logo & Main Menu */}
       <div className="flex items-center space-x-10">
-        <Image src="/img/mango-31-512.png" width={48} height={48} alt="MangoHotel" />
+        <Image
+          src="/img/mango-31-512.png"
+          width={48}
+          height={48}
+          alt="MangoHotel"
+        />
 
         <div className="hidden md:flex space-x-6">
-          <TopMenuItem title="MangoHotel" pageRef="/" className="text-lg font-bold text-indigo-500" />
-          <TopMenuItem title="Select Hotel" pageRef="/hotel" className="text-base font-semibold text-indigo-500 hover:text-indigo-700 " />
-          <TopMenuItem title="Booking" pageRef="/booking" className="text-base font-semibold text-indigo-500 hover:text-indigo-700" />
+          <TopMenuItem
+            title="MangoHotel"
+            pageRef="/"
+            className="text-lg font-bold text-indigo-500"
+          />
+          <TopMenuItem
+            title="Select Hotel"
+            pageRef="/hotel"
+            className="text-base font-semibold text-indigo-500 hover:text-indigo-700 "
+          />
+          <TopMenuItem
+            title="Booking"
+            pageRef="/booking"
+            className="text-base font-semibold text-indigo-500 hover:text-indigo-700"
+          />
         </div>
       </div>
 
       {/* Right Section: User Authentication & Profile */}
       <div className="flex items-center space-x-7">
-        <TopMenuItem title="My Booking" pageRef="/mybooking" className="text-base font-semibold text-indigo-500 hover:text-indigo-700" />
+        <TopMenuItem
+          title="My Booking"
+          pageRef="/mybooking"
+          className="text-base font-semibold text-indigo-500 hover:text-indigo-700"
+        />
 
         {session ? (
           <div className="flex items-center space-x-5 border-2 p1-2 border-indigo-500 rounded-full">
-            <TopMenuItem title="Sign Out" pageRef="/auth/signout" className="text-base font-semibold text-white bg-indigo-500 py-1 px-3 hover:bg-indigo-700 ml-2 rounded-full"/>
-            <Link href={'/profile'}>
-              <FaCircleUser className="text-4xl text-indigo-500 m-1"/>
+            <TopMenuItem
+              title="Sign Out"
+              pageRef="/auth/signout"
+              className="text-base font-semibold text-white bg-indigo-500 py-1 px-3 hover:bg-indigo-700 ml-2 rounded-full"
+            />
+            <Link href={"/profile"}>
+              <FaCircleUser className="text-4xl text-indigo-500 m-1" />
             </Link>
-            
           </div>
         ) : (
           <div className="flex space-x-4">
