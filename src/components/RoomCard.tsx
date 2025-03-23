8c35f0aee2c5b0feb46110f356c529369b41e44e
Chandan Kanyarat <6733029621@student.chula.ac.th>
2025-03-23 12:59:15 +0700

    Modified RoomCard

--- src/components/RoomCard.tsx
+++ src/components/RoomCard.tsx
@@ -27,14 +27,14 @@ export default async function RoomCard({
   const [value, setValue] = useState<number | null>(0);
   const router = useRouter();
   return (
-    <InteractiveCard className="w-1/5 h-[300px]">
+    <InteractiveCard className="w-1/5 h-[375px]">
       <div
       className="w-full h-full bg-[#E6EBF9] rounded-lg"
       style={{
         fontFamily: "sans-serif",
       }}
       >
-      <div className="relative w-full h-1/2">
+      <div className="relative w-full h-3/5">
         <Image
           src={imgSrc}
           alt="Product Picture"
@@ -42,32 +42,38 @@ export default async function RoomCard({
           className="object-cover rounded-t-lg"
         />
       </div>
-      <div className="px-4 pt-2">
-        <div className="flex items-center text-[26px] font-bold text-black">
-          ${pricePerNight} <span className="text-sm font-normal text-gray-700 mt-1">/night</span>
+      <div className="px-2 pt-2">
+        <div className="flex items-center text-[26px] font-bold text-black mx-2">
+          ${pricePerNight} <span className="text-sm font-normal text-black mt-1">/night</span>
         </div>
-        <hr className="mt-2 border-gray-300" />
+        <hr className="mt-1 border-gray-300 border-[1px]" />
       </div>
 
       {/* Size + People */}
-      <div className="flex flex-col justify-between px-4 pb-3 mt-10 text-sm text-gray-500">
-        <div className="flex items-center">
+      <div className="flex flex-col justify-between mt-2 px-4 text-[14px] text-gray-500 ">
+        <div className="flex items-center font-bold">
           <img
             src="/img/ruler.svg"
             alt="Location Icon"
             className="w-4 h-4 mr-1"
           />{" "}
-          {size} sqft
+          <span className="text-gray-700 mr-1 font-bold">{size}</span>sqft
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center font-bold">
           <img
             src="/img/user.png"
             alt="Location Icon"
             className="w-4 h-4 mr-1"
           />{" "}
-          adult: {adult} chldren: {children}
+          adult: <span className="text-gray-700 mr-1 ml-1 font-bold">{adult}</span> children: <span className="text-gray-700 mr-1 ml-1 font-bold-800">{children}</span>
         </div>
       </div>
+      <button
+        onClick={() => router.push(`/book/${hid}`)}
+        className="self-start mt-3 bg-yellow-400 hover:bg-yellow-500 text-white text-sm py-1 px-4 rounded-md"
+      >
+        Book
+      </button>
     </div>
     </InteractiveCard>
   );
